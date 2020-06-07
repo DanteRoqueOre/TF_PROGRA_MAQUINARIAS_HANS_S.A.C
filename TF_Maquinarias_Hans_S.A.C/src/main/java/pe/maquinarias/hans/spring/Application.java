49b28f29f30d40ee721c35d8a694c3fe469ba4d1
Dante Roque <daniel_ro1_@hotmail.com>
2020-06-06 20:22:06 -0500

    First Comit.

---
+++ TF_Maquinarias_Hans_S.A.C/src/main/java/pe/maquinarias/hans/spring/Application.java
@@ -0,0 +1,13 @@
+package pe.maquinarias.hans.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
